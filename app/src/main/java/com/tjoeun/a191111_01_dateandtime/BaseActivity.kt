8045e62610b0fc79babea369a43fa8821255058b
ducho <ducho5358@gmail.com>
2019-11-11 19:42:23 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a191111_01_dateandtime/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.tjoeun.a191111_01_dateandtime
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity{
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+}
